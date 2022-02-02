6471572a844716229f80720f2b65a87425090ed1
Marce Melgar <marcelamelgar@office.ufm.edu>
2022-02-02 11:35:51 -0600

    en funciones

--- Practice2/unidimensionales.py
+++ Practice2/unidimensionales.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import cProfile
 
 debitos = []
 creditos = []
@@ -26,6 +27,40 @@ def regresoMenu():
 def Promedio(cuenta):
     return np.sum(cuenta) / cuenta.size
 
+def ingresoDebitos(debitos):
+    debitos = np.array(debitos)
+    return(debitos)
+
+def ingresoCreditos(creditos):
+    creditos = np.array(creditos)
+    return(creditos)
+
+def sumDebitos(debitos):
+    return(np.sum(debitos))
+
+def sumCreditos(creditos):
+    return(np.sum(creditos))
+
+def getSaldo(creditos, debitos):
+    return(saldo)
+
+def getPromedio(debitos):
+    return(promedio)
+
+def getMax(debitos):
+    return(np.max(debitos))
+
+def operaciones(debitos, creditos):
+    return(debitos.size, creditos.size)
+
+def getEstado(debitos, creditos):
+    for i in range(debitos.size):
+        return(f"\t{debitos[i]}\n")
+    for j in range(creditos.size):
+        return(f"\t{creditos[j]}\n")
+
+
+
 while True:
     menu()
 
@@ -44,7 +79,7 @@ while True:
             debitos.append(deb)
             x += 1
         
-        debitos = np.array(debitos)
+        ingresoDebitos(debitos)
         print("Los debitos han sido registrados!\n")
 
         regresoMenu()
@@ -62,7 +97,7 @@ while True:
             creditos.append(cred)
             y += 1
         
-        creditos = np.array(creditos)
+        ingresoCreditos(creditos)
         print("Los creditos han sido registrados!\n")
         regresoMenu()
 
@@ -70,12 +105,14 @@ while True:
         os.system('clear')
         print("SUMA DE DEBITOS")
         print(f"\nLa suma total de debitos es: {np.sum(debitos)}\n")
+        sumDebitos(ingresoDebitos(debitos))
         regresoMenu()
 
     elif(opcion == 4):
         os.system('clear')
         print("SUMA DE CREDITOS")
         print(f"\nLa suma total de creditos es: {np.sum(creditos)}\n")
+        sumCreditos(ingresoCreditos(creditos))
         regresoMenu()
 
     elif(opcion == 5):
@@ -83,26 +120,30 @@ while True:
         saldo = np.sum(creditos) - np.sum(debitos)
         print("SALDO\n")
         print(f"El saldo es: {saldo}\n")
+        getSaldo(ingresoCreditos(creditos),ingresoDebitos(debitos))
         regresoMenu()
 
     elif(opcion == 6):
         os.system('clear')
         print("PROMEDIO DE DEBITOS\n")
-        promedio =  Promedio(debitos)
+        promedio =  Promedio(ingresoDebitos(debitos))
         print(f"El promedio de los debitos es: {round(promedio,2)}\n")
+        getPromedio(ingresoDebitos(debitos))
         regresoMenu()
 
     elif(opcion == 7):
         os.system('clear')
         print("MONTO MAYOR DE DEBITOS\n")
-        print(f"El monto en debitos mas grande es: {np.max(debitos)}\n")
+        print(f"El monto en debitos mas grande es: {np.max(ingresoDebitos(debitos))}\n")
+        getMax(ingresoDebitos(debitos))
         regresoMenu()
 
     elif(opcion == 8):
         os.system('clear')
         print("OPERACIONES REALIZADAS\n")
-        print(f"debitos: {debitos.size}")
-        print(f"creditos: {creditos.size}\n")
+        print(f"debitos: {ingresoDebitos(debitos).size}")
+        print(f"creditos: {ingresoCreditos(creditos).size}\n")
+        operaciones(ingresoDebitos(debitos),ingresoCreditos(creditos))
         regresoMenu()
 
     elif(opcion == 9):
@@ -110,20 +151,21 @@ while True:
         print("ESTADO DE CUENTA\n")
         print("\tDebitos")
         print("~~~~~~~~~~~~~~~~~~~~~")
-        for i in range(debitos.size):
-            print(f"\t{debitos[i]}\n")
+        for i in range(ingresoDebitos(debitos).size):
+            print(f"\t{ingresoDebitos(debitos)[i]}\n")
         print("\n\tCreditos")
         print("~~~~~~~~~~~~~~~~~~~~~")
-        for j in range(creditos.size):
-            print(f"\t{creditos[j]}\n")
+        for j in range(ingresoCreditos(creditos).size):
+            print(f"\t{ingresoCreditos(creditos)[j]}\n")
+        getEstado(ingresoDebitos(debitos), ingresoCreditos(creditos))
         regresoMenu()
 
     elif(opcion == 10):
         os.system('clear')
         print("\tCreditos")
         print("~~~~~~~~~~~~~~~~~~~~~")
-        for j in range(creditos.size):
-            print(f"\t{creditos[j]}")
+        for j in range(ingresoCreditos(creditos).size):
+            print(f"\t{ingresoCreditos(creditos)[j]}")
 
         print("\nSE ELIMINA CREDITO\n")
         salir = 's'
@@ -131,30 +173,31 @@ while True:
         while (salir == 's'):
     
             borrar = float(input("ingrese el monto del credito que quiere eliminar: "))
-            creditos = np.delete(creditos, np.where(creditos == borrar))
+            creditos = np.delete(ingresoCreditos(creditos), np.where(ingresoCreditos(creditos) == borrar))
+            ingresoCreditos(creditos)
 
             print("----------------------------------------")
             print("Total Debitos: ")
-            print(f"{np.sum(debitos)}\n")
+            print(f"{np.sum(ingresoDebitos(debitos))}\n")
             print("Total Creditos: ")
-            print(f"{np.sum(creditos)}\n")
+            print(f"{np.sum(ingresoCreditos(creditos))}\n")
             print("Saldo: ")
-            saldo = np.sum(creditos) - np.sum(debitos)
+            saldo = np.sum(ingresoCreditos(creditos)) - np.sum(ingresoDebitos(debitos))
             print(f"{saldo}\n")
             print("Promedio Debitos: ")
-            promedio =  Promedio(debitos)
+            promedio =  Promedio(ingresoDebitos(debitos))
             print(f"{round(promedio,2)}\n")
             print("Debito Mayor\n")
-            print(f"{np.max(debitos)}\n")
+            print(f"{np.max(ingresoDebitos(debitos))}\n")
             print("Estado de Cuenta:\n")
             print("\tDebitos")
             print("~~~~~~~~~~~~~~~~~~~~~")
-            for i in range(debitos.size):
-                print(f"\t{debitos[i]}\n")
+            for i in range(ingresoDebitos(debitos).size):
+                print(f"\t{ingresoDebitos(debitos)[i]}\n")
             print("\n\tCreditos")
             print("~~~~~~~~~~~~~~~~~~~~~")
-            for j in range(creditos.size):
-                print(f"\t{creditos[j]}\n")
+            for j in range(ingresoCreditos(creditos).size):
+                print(f"\t{ingresoCreditos(creditos)[j]}\n")
 
             print("----------------------------------------")
 
